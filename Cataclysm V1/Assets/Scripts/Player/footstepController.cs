7b1218da68a4d9173ad43ff1421f7ceb87220734
copingloki <coping.loki@gmail.com>
2024-01-24 13:02:19 -0500

    FootstepScriptForDungeonPerfect

--- Cataclysm V1/Assets/Scripts/Player/footstepController.cs
+++ Cataclysm V1/Assets/Scripts/Player/footstepController.cs
@@ -1,109 +1,60 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class footstepController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public AudioSource footstepsDungeon;
-    public AudioSource footstepsDungeon2;
-    public AudioSource footstepsDungeon3;
-    public AudioSource footstepsDungeon4;
-
     public AudioClip[] sounds; // Array to hold your AudioClips
     private AudioSource audioSource;
     private WaitForSeconds waitTime = new WaitForSeconds(1f);
+    private Coroutine footstepCoroutine; // Reference to the coroutine
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
-        // Call the coroutine to play random sounds
-        
     }
+
     // Update is called once per frame
     void Update()
     {
         if (InputManager.disabled == false)
         {
-
-            //Debug.Log(portalTeleporterScript);
-
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
             {
-                //if (SceneManager.GetActiveScene().name == "Dungeon"){
-                //Debug.Log("moving");
-                //StartCoroutine("footstepsAudio");
-                StartCoroutine(PlayRandomSounds());
+                if (footstepCoroutine == null)
+                {
+                    footstepCoroutine = StartCoroutine(PlayRandomSounds());
+                }
             }
             else
             {
-                StopCoroutine("footstepsAudio");
-                footstepsDungeon.enabled = false;
-                footstepsDungeon2.enabled = false;
-                footstepsDungeon3.enabled = false;
-                footstepsDungeon4.enabled = false;
-
-
-
-
+                if (footstepCoroutine != null)
+                {
+                    StopCoroutine(footstepCoroutine);
+                    footstepCoroutine = null;
+                }
             }
         }
-        else footstepsDungeon.enabled = false;
     }
 
-    IEnumerator footstepsAudio()
-    {
-            footstepsDungeon.enabled = true;
-            footstepsDungeon3.enabled = false;
-            yield return new WaitForSeconds(0.75f);
-            footstepsDungeon2.enabled = true;
-            footstepsDungeon4.enabled = false;
-            yield return new WaitForSeconds(0.75f);
-            footstepsDungeon3.enabled = true;
-            footstepsDungeon.enabled = false;
-            yield return new WaitForSeconds(.75f);
-            footstepsDungeon4.enabled = true;
-            footstepsDungeon2.enabled = true;
-            yield return new WaitForSeconds(0.75f);
-
-
-
-
-    }
     IEnumerator PlayRandomSounds()
     {
         while (true)
         {
-            // Pick a random AudioClip from the array
             int randomIndex = Random.Range(0, sounds.Length);
             AudioClip selectedSound = sounds[randomIndex];
-
-            // Play the selected sound using the AudioSource
+            audioSource.volume = Random.Range(0.02f, 0.05f);
+            audioSource.pitch = Random.Range(0.8f, 1.2f);
             audioSource.PlayOneShot(selectedSound);
-
-            // Wait for a second
             yield return waitTime;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other!=null)
+        if (other != null && footstepCoroutine != null)
         {
-            StopCoroutine("footstepsAudio");
-
-
+            StopCoroutine(footstepCoroutine);
+            footstepCoroutine = null;
         }
     }
-
-
 }
-
-
-
-
-
-
-
